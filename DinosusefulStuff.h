82826190ad5896ac41deebcb63b5bb47d5c83e43
Dino Pollano <cpollano@gmail.com>
2017-08-20 22:08:25 +0100

    added IFFTWrapper
    not entirely sure it works

--- DinosusefulStuff.h
+++ DinosusefulStuff.h
@@ -10,5 +10,6 @@
 #define dinosUsefulStuff_h
 #include "circBuffer/src/circularBuffer.hpp"
 #include "fftWrapper/src/FFTWrapper.h"
+#include "fftWrapper/src/IFFTWrapper.h"
 
 #endif /* dinosUsefulStuff_h */

c6ea3edb93e595edb6ef7f5e95d5c083c4b75b2e
Dino Pollano <cpollano@gmail.com>
2017-11-25 16:33:46 +0000

    beginningings

---
+++ filterMaker/FilterMaker/FilterMaker/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FilterMaker
+//
+//  Created by Christofero Pollano on 25/11/2017.
+//  Copyright © 2017 dino. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hh"
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//  // insert code here...
+//  std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
